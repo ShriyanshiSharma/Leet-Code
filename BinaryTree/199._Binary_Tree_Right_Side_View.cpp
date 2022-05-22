24283ae7e97db01a12c7b42a3635d8eb66f0d528
Shriyanshi Sharma <shriyanshi.19b131153@abes.ac.in>
2022-05-22 15:20:38 +0530

    199. Binary Tree Right Side View

---
+++ BinaryTree/199._Binary_Tree_Right_Side_View.cpp
@@ -0,0 +1,37 @@
+// 199. Binary Tree Right Side View
+
+/*
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+Example 1:
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+
+Example 2:
+Input: root = [1,null,3]
+Output: [1,3]
+
+Example 3:
+Input: root = []
+Output: []
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+class Solution {
+public:
+    void right(TreeNode* root, vector<int> &ans ,int l ){
+        if(root == NULL) return;
+        if(l == ans.size()) ans.push_back(root->val);
+        right(root->right,ans,l+1);
+        right(root->left,ans,l+1);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        right(root,ans,0);
+        return ans;
+    }
+};
